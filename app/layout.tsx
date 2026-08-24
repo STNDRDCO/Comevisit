edd0ce8f075914ed42bd6c6f783c92826b2e0939
STNDRDCO <jlevykevin@gmail.com>
2026-08-23 21:16:20 -0300

    polish: set app document language to es-AR

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,5 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><body>{children}</body></html>;
+  return <html lang="es-AR"><body>{children}</body></html>;
 }
